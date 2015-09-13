9f42ac20360e86985b1430b5da6bddb116f17622
Niklas Grahl <niklas.grahl@gmail.com>
2015-09-13 22:09:06 +0200

    first attempt at task 4. Not finished.

--- tasks.js
+++ tasks.js
@@ -129,6 +129,69 @@
   add_ = defaultArguments(add_, { c: 3 }); // doesn't do anything, since c isn't an argument
   console.assert(add_(10) === NaN); // returns NaN
   console.assert(add_(10, 10) === 20); // returns 20
+});
+
+// Task 4
+
+(function(){
+  const START_TIME = 9 * 60; // 9:00
+  const MAX_TIME = 600; // 19:00
+
+  function timeStrToNumber(timeStr){
+    const s = timeStr.split(':');
+    return (+s[0]) * 60 + (+s[1]) - START_TIME;
+  }
+  function numberToTimeStr(number){
+    const minute = number % 60;
+    const hour = (START_TIME + number - minute) / 60;
+    return hour + ":" + minute;
+  }
+
+  function areSchedulesFree(schedules, start, minutes){
+    return schedules.every(s => {
+      return s.every(s2 => {
+        return (s2[0] >= start + minutes || s2[1] <= start);
+      });
+    });
+  }
+  
+  function numberifySchedule(schedule){
+    return schedule.map(s => {
+      return [timeStrToNumber(s[0]), timeStrToNumber(s[1])];
+    });
+  }
+
+  function firstAppointment(schedules, minutes){
+    let currentTime = 0;
+    let found = null;
+    let schedulesNbr = schedules.map(numberifySchedule);
+
+    while(!found && currentTime < MAX_TIME){
+      if(areSchedulesFree(schedulesNbr, currentTime, minutes)){
+        found = currentTime;
+      }else{
+        currentTime += schedulesNbr.reduce((prev, curr) => {
+          let time = curr.reduce(a => {
+            let endTime = a[1];
+            return (prev && endTime >= prev) ? prev : endTime;
+          });
+          return Math.min(prev, time);
+        });
+      }
+    }
+    return found;
+  }
+
+  let schedules = [
+    [['09:00', '11:30'], ['13:30', '16:00'], ['16:00', '17:30'], ['17:45', '19:00']],
+    [['09:15', '12:00'], ['14:00', '16:30'], ['17:00', '17:30']],
+    [['11:30', '12:15'], ['15:00', '16:30'], ['17:45', '19:00']]
+  ];
+
+  console.assert(timeStrToNumber('12:15') === 195);
+  console.assert(numberToTimeStr(195) === '12:15');
+  console.assert(numberifySchedule(schedules[0])[1][1] === 420);
+  console.assert(areSchedulesFree(schedules.map(numberifySchedule), 195, 10));
+  console.assert(!areSchedulesFree(schedules.map(numberifySchedule), 190, 10));
+  //console.assert(firstAppointment(schedules, 60) === '12:15');
 })();
-
-
